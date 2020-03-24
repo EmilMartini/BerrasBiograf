6da876fa898beae100df16e907f993096221aed8
unknown <daniel.andersson@iths.se>
2020-03-24 15:42:31 +0100

    minor changes

--- Controllers/ViewingsController.cs
+++ Controllers/ViewingsController.cs
@@ -52,13 +52,23 @@ namespace BerrasBiograf.Controllers
         }
 
         //BOKA BIO VY-CALL
-        //public async Task<IActionResult> BookViewing(Guid? id)
-        //{
-        //    if(id == null)
-        //    {
-        //        return NotFound();
-        //    }
-        //}
+        public async Task<IActionResult> Edit(Guid? id)
+        {
+            if(id == null)
+            {
+                return NotFound();
+            }
+
+
+            var viewing = await _context.Viewings.FindAsync(id);
+            if (viewing == null)
+            {
+                return NotFound();
+            }
+            viewing.AvailableSeats--;
+            return View(viewing);
+
+        }
 
         // GET: Viewings/Create
 
@@ -100,35 +110,35 @@ namespace BerrasBiograf.Controllers
         //// more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         //[HttpPost]
         //[ValidateAntiForgeryToken]
-        //public async Task<IActionResult> Edit(Guid id, [Bind("Id,AvailableSeats,TimeOfScreening")] Viewing viewing)
-        //{
-        //    if (id != viewing.Id)
-        //    {
-        //        return NotFound();
-        //    }
-        //
-        //    if (ModelState.IsValid)
-        //    {
-        //        try
-        //        {
-        //            _context.Update(viewing);
-        //            await _context.SaveChangesAsync();
-        //        }
-        //        catch (DbUpdateConcurrencyException)
-        //        {
-        //            if (!ViewingExists(viewing.Id))
-        //            {
-        //                return NotFound();
-        //            }
-        //            else
-        //            {
-        //                throw;
-        //            }
-        //        }
-        //        return RedirectToAction(nameof(Index));
-        //    }
-        //    return View(viewing);
-        //}
+        public async Task<IActionResult> BookViewing(Guid id, [Bind("Id,AvailableSeats")] Viewing viewing)
+        {
+            if (id != viewing.Id)
+            {
+                return NotFound();
+            }
+        
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(viewing.AvailableSeats--);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ViewingExists(viewing.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(viewing);
+        }
         //
         //// GET: Viewings/Delete/5
         //public async Task<IActionResult> Delete(Guid? id)
