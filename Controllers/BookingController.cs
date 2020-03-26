b41ef9259e399da93a90db818adbfaa465825b48
Emil Martini <emil.martini@hotmail.com>
2020-03-26 22:28:04 +0100

    add ability to add booking to current signed in user

--- Controllers/BookingController.cs
+++ Controllers/BookingController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Linq;
 using System.Threading.Tasks;
@@ -22,7 +23,18 @@ namespace BerrasBiograf
         [Authorize]
         public async Task<IActionResult> Index()
         {
-            return View();
+            var currentUser = await _userManager.GetUserAsync(User);
+
+            var bookings = from booking in await _context.Bookings.ToListAsync()
+                           where booking.User == currentUser
+                           join viewing in await _context.Viewings.ToListAsync() on booking.Viewing.Id equals viewing.Id
+                           join movie in await _context.Movies.ToListAsync() on viewing.MovieToShow.Id equals movie.Id
+                           join locale in await _context.Locales.ToListAsync() on viewing.LocaleToShow.Id equals locale.Id
+                           join user in await _context.Users.ToListAsync() on booking.User equals user
+                           orderby booking.TimeOfBooking ascending
+                           select booking;
+
+            return View(bookings);
         }
 
         [HttpPost]
@@ -41,6 +53,30 @@ namespace BerrasBiograf
 
             await _context.SaveChangesAsync();
             return View(viewing);
+        }
+
+
+        public async Task<IActionResult> AddBooking(Guid? id, AddBookingModel bookingModel)
+        {
+            if(id == null)
+            {
+                return NotFound();
+            }
+
+            var viewing = await _context.Viewings.FindAsync(id);
+            var user = await _userManager.GetUserAsync(User);
+
+            var booking = new Booking
+            {
+                Id = new Guid(),
+                Viewing = viewing,
+                User = user,
+                NumberOfBookedSeats = 10,
+                TimeOfBooking = DateTime.Now
+            };
+            _context.Bookings.Add(booking);
+            await _context.SaveChangesAsync();
+            return RedirectToAction("Index");
         }
     }
 }
