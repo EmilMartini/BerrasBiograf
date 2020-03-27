95d5716816354afa6c1fee5fe20f2600f6c739c9
Daniel <daniel.andersson@iths.se>
2020-03-27 07:03:51 +0100

    minor changes

--- Controllers/ViewingsController.cs
+++ Controllers/ViewingsController.cs
@@ -93,59 +93,13 @@ namespace BerrasBiograf
         //// POST: Viewings/Edit/5
         //// To protect from overposting attacks, please enable the specific properties you want to bind to, for 
         //// more details see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [Authorize]
-        public async Task<IActionResult> BookViewing(Guid? id)
-        {
-            if(id == null)
-            {
-                return NotFound();
-            }
-            var viewing = await _context.Viewings.FindAsync(id);
-            if (viewing == null)
-            {
-                return NotFound();
-            }
-            viewing.AvailableSeats--;
-            await _context.SaveChangesAsync();
-            return View(viewing);
-        }
-
-
-
-
-
-        [HttpPost]
-        //[ValidateAntiForgeryToken]
-        public async Task<IActionResult> BookViewing2(Guid id, [Bind("Id,AvailableSeats")] Viewing viewing)
-        {
-            if (id != viewing.Id)
-            {
-                return NotFound();
-            }
         
-            if (ModelState.IsValid)
-            {
-                try
-                {
-                    _context.Update(viewing.AvailableSeats--);
-                    await _context.SaveChangesAsync();
-                }
-                catch (DbUpdateConcurrencyException)
-                {
-                    if (!ViewingExists(viewing.Id))
-                    {
-                        return NotFound();
-                    }
-                    else
-                    {
-                        throw;
-                    }
-                }
-                return RedirectToAction(nameof(Index));
-            }
-            return View(viewing);
-        }
+
+
+
+
+
+        
         //
         //// GET: Viewings/Delete/5
         //public async Task<IActionResult> Delete(Guid? id)
